9f1ceac2462e9151fea60f56a402e4aa74ca4981
keyserfaty <keserfaty@gmail.com>
2017-07-08 19:07:46 -0300

    add search when using other search box too

--- src/components/SearchBoxHeader.js
+++ src/components/SearchBoxHeader.js
@@ -11,11 +11,22 @@ const SearchBoxHeader = props => {
     dispatch({ type: 'ON_HEADER_SEARCH_BOX_BLUR' })
   }
 
+  const onKeyupInput = e => {
+    if (e.keyCode === 13) {
+      dispatch({
+        type: 'ON_INPUT_ENTER_KEY_DOWN',
+        payload: {
+          searchValue: e.target.value,
+        }
+      })
+    }
+  }
+
   return (
     Node('div', { class: 'search' },
       Node('div', { class: 'search-box' },
         Node('i', { class: 'icon' }),
-        Node('input', { type: 'text', value: searchValue, onFocus: onSearchBoxFocus, onBlur: onSearchBoxBlur })
+        Node('input', { type: 'text', value: searchValue, onKeyUp: onKeyupInput, onFocus: onSearchBoxFocus, onBlur: onSearchBoxBlur })
       )
     )
   )
