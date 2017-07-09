017ec5951ee40e1e8fd6ce6aafac4c8a862d2bb1
keyserfaty <keserfaty@gmail.com>
2017-07-08 23:07:22 -0300

    separate reducer from app file

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,1 +1,22 @@
 export const exists = node => node != null
+
+const imageLoader = src => {
+  return new Promise(function (resolve, reject) {
+    let img = new Image()
+    img.onload = function () {
+      resolve(src)
+    }
+    img.onerror = function (err) {
+      reject(err)
+    }
+    img.src = src
+  })
+}
+
+/**
+ * Takes a list of images urls and returns a list
+ * of promises of images loading
+ * @param images
+ */
+export const preCachedImages = images => images
+  .map(image => imageLoader(image))
---
+++ src/reducer.js
@@ -0,0 +1,142 @@
+import { createStore } from './helpers/store'
+
+// model
+const initialState = {
+  // routing
+  routes: {
+    path: '/'
+  },
+
+  // ui
+  searchValue: '',
+  displayDownloads: false,
+  isNavigating: false,
+  imagesQueue: [],
+  currentImage: null,
+  currentImageId: 0,
+  nextImage: null,
+
+  // async
+  imagesList: [],
+  currentPage: null,
+  totalPages: null,
+  totalImages: null
+}
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'ON_INPUT_ENTER_KEY_DOWN': {
+      return {
+        ...state,
+        searchValue: action.payload.searchValue,
+        routes: {
+          path: '/search'
+        }
+      }
+    }
+
+    case 'ON_HEADER_SEARCH_BOX_FOCUS': {
+      return {
+        ...state,
+        isNavigating: false,
+      }
+    }
+
+    case 'ON_HEADER_SEARCH_BOX_BLUR': {
+      return {
+        ...state,
+        isNavigating: true,
+      }
+    }
+
+    case 'ON_TOGGLE_DOWNLOADS': {
+      return {
+        ...state,
+        displayDownloads: !state.displayDownloads,
+      }
+    }
+
+    case 'ON_CLEAR_ALL_DOWNLOADS': {
+      return {
+        ...state,
+        imagesQueue: [],
+      }
+    }
+
+    case 'ON_ADD_IMAGE_TO_QUEUE': {
+      return {
+        ...state,
+        imagesQueue: [
+          ...state.imagesQueue,
+          state.currentImage
+        ]
+      }
+    }
+
+    case 'ON_REMOVE_IMAGE_FROM_QUEUE': {
+      return {
+        ...state,
+        imagesQueue: state.imagesQueue
+        .filter((image, i) => i !== Number(action.payload.id))
+      }
+    }
+
+    case 'ON_END_OF_LIST': {
+      return {
+        ...state,
+        currentImageId: 0
+      }
+    }
+
+    case 'ON_FETCH_IMAGES': {
+      return {
+        ...state,
+        currentPage: action.payload.currentPage
+      }
+    }
+
+    case 'ON_FETCH_IMAGES_SUCCESS': {
+      const isNewRequest = state.currentPage === 1
+
+      const currentImageId = isNewRequest
+        ? 0
+        : state.currentImageId
+
+      const imagesList = isNewRequest
+        ? action.payload.images.results
+        : [
+          ...state.imagesList,
+          ...action.payload.images.results
+        ]
+
+      const currentImage = isNewRequest
+        ? action.payload.images.results[currentImageId]
+        : state.imagesList[currentImageId]
+
+      return {
+        ...state,
+        imagesList,
+        currentImage,
+        currentImageId,
+        isNavigating: true,
+        nextImage: action.payload.images.results[currentImageId + 1],
+        totalPages: action.payload.images.total_pages,
+        totalImages: action.payload.images.total
+      }
+    }
+
+    case 'ON_LOAD_NEXT_IMAGE': {
+      return {
+        ...state,
+        currentImage: state.imagesList[state.currentImageId + 1],
+        nextImage: state.imagesList[state.currentImageId + 2],
+        currentImageId: state.currentImageId + 1
+      }
+    }
+
+    default:
+      return state
+  }
+}
+
+export const store = createStore(reducer)
